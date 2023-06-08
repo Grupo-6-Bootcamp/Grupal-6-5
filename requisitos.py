db32e98b653a52f7cea9c212d1e6b8bfdfefeb30
Guido Alejandro Saavedra Torres <galejandrosaavedra@gmail.com>
2023-06-08 14:31:20 -0400

    first commit

---
+++ requisitos.py
@@ -0,0 +1,10 @@
+# Continuando con su proyecto, desarrolle toda la lógica necesaria para que los usuarios se puedan
+# registrar.
+# Elaboren un template de login y un template de bienvenida posterior al login. Este último template debe
+# mostrar un mensaje personalizado, saludando a quien haya iniciado sesión.
+# Si el usuario no ha iniciado sesión, bajo ninguna circunstancia podrá ver la página de bienvenida
+# personalizada. Restringe los permisos correspondientes.
+# Elaboren un archivo README.txt donde se expliquen las funcionalidades de su aplicación y toda la
+# información necesaria para que cualquier desarrollador pueda levantar su aplicación en un servidor
+# local.
+# Actualice su repositorio en GitHub y envielo en el archivo comprimido con su proyecto.
